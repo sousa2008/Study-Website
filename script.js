a2009f6e9d9f7961d1155932f2b9eff46fe33044
Lucas de Sousa <lucas.2008.sousa@gmail.com>
2026-04-02 12:27:24 -0300

    V2.1: add new task (beta)

--- script.js
+++ script.js
@@ -17,6 +17,48 @@ function mudarNome (){
         mensagemErro.style.color = 'green'
         usuario.value = " " // Limpar o campo de input após a atualização do nome   
     }
+}
+
+function adicionartarefa(){
+    // ler a tarefa que o usuário digitar
+    let materia = document.getElementById("materia")
+    let novaTarefa = materia.value
+    
+    let data = document.getElementById("data")
+    let novaData = data.value
+    
+    // Declarar o paragrafo de erro 
+    let mensagemErro2 = document.getElementById("mensagemErro2")
+    
+    
+    if (novaTarefa.trim() === "" || novaData.trim() === "") {
+        mensagemErro2.textContent = "Digite uma matéria válida!"
+    }
+    else {
+    //Craindo cada parte que sera adicionada na lista de tarefas
+    let lista = document.createElement("li")
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    let span =document.createElement("span")
+    span.textContent = novaTarefa
+
+    /////adcionando na linha da lista 
+    lista.appendChild(checkbox) // Adicionar o checkbox à tarefa
+    lista.appendChild(span) // Adicionar o texto da tarefa ao elemento de lista
+    // Adicionar o elemento de lista à lista de tarefas
+    let listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.appendChild(lista)
+
+    /* lista para adcionar data junto (esperando aprender mais para poder fazer)
+    let lista2 = document.createElement('li')
+    lista2.textContent = novaData + " - " + novaTarefa
+    let listaDatas = document.getElementById("listaDatas")
+    listaDatas.appendChild(lista2) */
+    
+    mensagemErro2.textContent = "Atividade adicionada com sucesso!"
+    usuario.value = " "
+}
+
 
 }
 
