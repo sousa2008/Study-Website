9f7d7a32611d20ecfd329aee560bb681cdca0aac
Lucas de Sousa <lucas.2008.sousa@gmail.com>
2026-04-09 12:32:25 -0300

    V2.2: Added edit button and delet button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//durante o projeto, o indice do array é utilizado dversas vezes. 
+
 //criação da lista para agrupar tarefas
 let tarefas = []
 let datas = []
@@ -11,11 +13,11 @@ function mudarNome (){
     let mensagemErro = document.getElementById("mensagemErro")
     
     // Criar validação verificando se o nome está vazio 
-    if (novoNome.trim() === ""){  // trim similar ao strip do python
+    if (novoNome.trim() === ""){  
         mensagemErro.textContent = "Digite um nome válido!"
         mensagemErro.style.color = "red"
     }  
-    else {  //Código para quando o nome for válido
+    else {  
         document.getElementById("visitante").textContent = "SEJA BEM VINDO, " + novoNome + "!"
         mensagemErro.textContent = "Nome atualizado com sucesso!" 
         mensagemErro.style.color = 'green'
@@ -57,20 +59,33 @@ function exibirTarefas(){
     listaTarefas.innerHTML = "" //limpando a lista para evitar que as tarefas se repitam toda vez que uma nova for adicionada
 
     // laço de repetição para criar todas as linhas de tarefas 
-    let i = 0
-    for(i; i < tarefas.length; i++){
+    for(let i = 0; i < tarefas.length; i++){
         //Criando cada parte que sera adicionada na lista de tarefas
         let lista = document.createElement("li")
-        lista.className = "tarefa" //adicionando uma classe para cada linha criada, para facilitar a estilizaçãoS
+        lista.className = "tarefa" 
         let checkbox = document.createElement("input")
         checkbox.type = "checkbox"
-        checkbox.className = "checkbox" //adicionando uma classe para cada checkbox criado, para facilitar a estilização
+        checkbox.className = "checkbox" 
         let span = document.createElement("span")
         span.textContent = tarefas[i]
+        
+        //criando botão de excluir
+        let botaoExcluir = document.createElement("button")
+        botaoExcluir.textContent = "Excluir"
+        botaoExcluir.className = "botaoExcluir"
+        botaoExcluir.onclick = () => {limparTarefa(i)} //chamando a função de excluir tarefa, passando o indice da tarefa a ser excluida
+
+        //criando botão de editar
+        let botaoEditar = document.createElement("button")
+        botaoEditar.textContent = "Editar"
+        botaoEditar.className = "botaoEditar"
+        botaoEditar.onclick = () => {editarTarefa(i)} //chamando a função de editar tarefa, passando o indice da tarefa a ser editada
 
         // Confecção da linha e seus elementos (especie de agrupamento de cada parte da linha)
-        lista.appendChild(checkbox)  // Adicionar o checkbox à linha
-        lista.appendChild(span)  // Adicionar o texto da tarefa na linha
+        lista.appendChild(checkbox)
+        lista.appendChild(span)  
+        lista.appendChild(botaoExcluir)
+        lista.appendChild(botaoEditar) 
         
         // Adicionando todo o agrupamento feito na linha dentro da aba lista
         listaTarefas.appendChild(lista)
@@ -79,17 +94,43 @@ function exibirTarefas(){
 
 function exibirDatas(){
     const listaDatas = document.getElementById("listaDatas")
-    listaDatas.innerHTML = "" //limpando a lista para evitar que as tarefas se repitam toda vez que uma nova for adicionada
+    listaDatas.innerHTML = "" 
 
     // laço de repetição para criar todas as linhas de datas 
     i = 0
     for (i; i < datas.length; i++){
-        //// Confecção da linha de data e seus elementos
+        
+        // Confecção da linha de data e seus elementos
         lista = document.createElement('li')
         lista.textContent = datas[i] + " - " + tarefas[i]
-        //lista.style.color = "#25d164"
 
         //Adição do agrupamento da linha dentro da aba lista de datas
         listaDatas.appendChild(lista) 
     }
+}
+
+function limparTarefa(i){
+    tarefas.splice(i, 1)
+    datas.splice(i, 1)
+    exibirTarefas()
+    exibirDatas()
+    }
+
+function editarTarefa(i){
+    let tarefaEditada = prompt("Digite a nova tarefa:")
+    if (tarefaEditada.trim() !== ""){
+        tarefas[i] = tarefaEditada
+        exibirTarefas()
+        exibirDatas()
+    }
+
+}
+
+function limparTudo (){
+    tarefas.length = 0
+    datas.length = 0
+    mensagemErro2.textContent = "Lista limpa com sucesso!"
+    mensagemErro2.style.color = "blue"
+    exibirTarefas()
+    exibirDatas()
 }
