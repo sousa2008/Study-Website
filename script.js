cde4ba2a468292c2e8f61314fd5ebff8f4d77f34
Lucas de Sousa <lucas.2008.sousa@gmail.com>
2026-04-10 16:56:59 -0300

    V3.0: Added state system and interface cleaning

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 //criação da lista para agrupar tarefas
 let tarefas = []
 let datas = []
+let concluidas = []
 
 function mudarNome (){
     // Declarar o valor do input e puxar ele com "novoNome"
@@ -21,7 +22,7 @@ function mudarNome (){
         document.getElementById("visitante").textContent = "SEJA BEM VINDO, " + novoNome + "!"
         mensagemErro.textContent = "Nome atualizado com sucesso!" 
         mensagemErro.style.color = 'green'
-        usuario.value = " " // Limpar o campo de input após a atualização do nome   
+        usuario.value = "" // Limpar o campo de input após a atualização do nome   
     }
 }
 
@@ -43,31 +44,52 @@ function adicionartarefa(){
     else {
         tarefas.push(novaTarefa) //colocando a tarefa na array
         datas.push(novaData)
+        concluidas.push(false) //o push é falso, pois a tarefa não está concluida (checkado? falso)
         exibirTarefas()
         exibirDatas()
 
         //mensagem de sucesso e limpeza do campo de input
         mensagemErro2.textContent = "Atividade adicionada com sucesso!"
         mensagemErro2.style.color = "green"
-        materia.value = " "
-        data.value = " "
+        materia.value = ""
+        data.value = ""
 }
 }
 
 function exibirTarefas(){
     const listaTarefas = document.getElementById("listaTarefas")
     listaTarefas.innerHTML = "" //limpando a lista para evitar que as tarefas se repitam toda vez que uma nova for adicionada
+    
 
     // laço de repetição para criar todas as linhas de tarefas 
     for(let i = 0; i < tarefas.length; i++){
         //Criando cada parte que sera adicionada na lista de tarefas
         let lista = document.createElement("li")
         lista.className = "tarefa" 
+        
+       
+        let span = document.createElement("span")
+        span.textContent = tarefas[i]
+
         let checkbox = document.createElement("input")
         checkbox.type = "checkbox"
         checkbox.className = "checkbox" 
-        let span = document.createElement("span")
-        span.textContent = tarefas[i]
+        checkbox.checked = concluidas[i] //já definir se eatava cocluida antes de renderizar
+        checkbox.addEventListener("change", () =>{
+            concluidas[i] = checkbox.checked //a array recebe o valor do indice como true quando muda
+            if (concluidas[i] == true){
+                span.className = "tarefaConcluida"
+                span.classList.add = "tarefaConcluida" //classe dada as tarefas concluidas para alterar no css
+                listaDatas.className = "tarefaConcluida"
+                listaDatas.classList.add = "tarefaConcluida"
+
+            } 
+        else {
+                span.classList.remove("tarefaConcluida")
+                listaDatas.classList.remove("tarefaConcluida")
+            }
+        }) //atualiza ao mudar o estado da checkbox
+        
         
         //criando botão de excluir
         let botaoExcluir = document.createElement("button")
@@ -86,7 +108,7 @@ function exibirTarefas(){
         lista.appendChild(span)  
         lista.appendChild(botaoExcluir)
         lista.appendChild(botaoEditar) 
-        
+
         // Adicionando todo o agrupamento feito na linha dentro da aba lista
         listaTarefas.appendChild(lista)
 }
@@ -100,9 +122,13 @@ function exibirDatas(){
     i = 0
     for (i; i < datas.length; i++){
         
+        let dataFormatada = datas[i].split("-") //separando cada elemento da data 
+        let dia = dataFormatada[2]  //pegando cada parte da data pelo indice
+        let mes = dataFormatada[1]
+        
         // Confecção da linha de data e seus elementos
         lista = document.createElement('li')
-        lista.textContent = datas[i] + " - " + tarefas[i]
+        lista.textContent = dia + "/" + mes + " - " + tarefas[i]
 
         //Adição do agrupamento da linha dentro da aba lista de datas
         listaDatas.appendChild(lista) 
@@ -112,6 +138,7 @@ function exibirDatas(){
 function limparTarefa(i){
     tarefas.splice(i, 1)
     datas.splice(i, 1)
+    concluidas.splice(i, 1) //removendo o estado da tarefa excluida
     exibirTarefas()
     exibirDatas()
     }
@@ -127,8 +154,10 @@ function editarTarefa(i){
 }
 
 function limparTudo (){
+    const mensagemErro2 = document.getElementById("mensagemErro2")
     tarefas.length = 0
     datas.length = 0
+    concluidas.length = 0
     mensagemErro2.textContent = "Lista limpa com sucesso!"
     mensagemErro2.style.color = "blue"
     exibirTarefas()
