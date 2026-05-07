b299886882db3b74ffa14556d8a3851c8f099679
Lucas de Sousa <lucas.2008.sousa@gmail.com>
2026-05-07 12:52:00 -0300

    V3.1: Added localStorage System and bug fixes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 let tarefas = []
 let datas = []
 let concluidas = []
+let categorias = []
 
 function mudarNome (){
     // Declarar o valor do input e puxar ele com "novoNome"
@@ -33,33 +34,54 @@ function adicionartarefa(){
         
         const data = document.getElementById("data")
         let novaData = data.value
-    
-    
+
  // Declarar o paragrafo de erro 
         const mensagemErro2 = document.getElementById("mensagemErro2")
-    if (novaTarefa.trim() === "" || novaData.trim() === "") {
+    if (novaTarefa.trim() === "" || novaData.trim() === ""){
         mensagemErro2.textContent = "Digite uma matéria válida!"
         mensagemErro2.style.color = "red"
     }
     else {
+
+        //Colocando arrays no localStorage
+        if (localStorage.arr){
+            tarefas = JSON.parse(localStorage.getItem("arr"))
+        }
         tarefas.push(novaTarefa) //colocando a tarefa na array
+        localStorage.arr = JSON.stringify(tarefas) //jogando array para dentro do localstorage
+        
+
+        if (localStorage.arrDate){
+            datas = JSON.parse(localStorage.getItem("arrDate"))
+        }
         datas.push(novaData)
+        localStorage.arrDate = JSON.stringify(datas)
+        
         concluidas.push(false) //o push é falso, pois a tarefa não está concluida (checkado? falso)
+        localStorage.concluidas = JSON.stringify(concluidas)
+        
         exibirTarefas()
         exibirDatas()
+        atualizarContador()
+        tarefaRecente()
+        progresso()
 
         //mensagem de sucesso e limpeza do campo de input
         mensagemErro2.textContent = "Atividade adicionada com sucesso!"
         mensagemErro2.style.color = "green"
         materia.value = ""
         data.value = ""
+
+
+        return{mensagemErro2}
+        
 }
 }
 
 function exibirTarefas(){
     const listaTarefas = document.getElementById("listaTarefas")
     listaTarefas.innerHTML = "" //limpando a lista para evitar que as tarefas se repitam toda vez que uma nova for adicionada
-    
+    const listaDatas = document.getElementById("listaDatas")
 
     // laço de repetição para criar todas as linhas de tarefas 
     for(let i = 0; i < tarefas.length; i++){
@@ -78,15 +100,21 @@ function exibirTarefas(){
         checkbox.addEventListener("change", () =>{
             concluidas[i] = checkbox.checked //a array recebe o valor do indice como true quando muda
             if (concluidas[i] == true){
-                span.className = "tarefaConcluida"
-                span.classList.add = "tarefaConcluida" //classe dada as tarefas concluidas para alterar no css
-                listaDatas.className = "tarefaConcluida"
-                listaDatas.classList.add = "tarefaConcluida"
+                span.className = "tarefaConcluida" //classe dada as tarefas concluidas para alterar no css
+                listaDatas.children[i].className = "tarefaConcluida"
+
+                atualizarContador()
+                tarefaRecente()
+                progresso()
+
 
             } 
         else {
                 span.classList.remove("tarefaConcluida")
-                listaDatas.classList.remove("tarefaConcluida")
+                listaDatas.children[i].classList.remove("tarefaConcluida")
+                atualizarContador()
+                tarefaRecente()
+                progresso()
             }
         }) //atualiza ao mudar o estado da checkbox
         
@@ -111,6 +139,8 @@ function exibirTarefas(){
 
         // Adicionando todo o agrupamento feito na linha dentro da aba lista
         listaTarefas.appendChild(lista)
+
+
 }
 }
 
@@ -119,8 +149,8 @@ function exibirDatas(){
     listaDatas.innerHTML = "" 
 
     // laço de repetição para criar todas as linhas de datas 
-    i = 0
-    for (i; i < datas.length; i++){
+
+    for (i = 0; i < datas.length; i++){
         
         let dataFormatada = datas[i].split("-") //separando cada elemento da data 
         let dia = dataFormatada[2]  //pegando cada parte da data pelo indice
@@ -137,10 +167,18 @@ function exibirDatas(){
 
 function limparTarefa(i){
     tarefas.splice(i, 1)
+    localStorage.arr = JSON.stringify(tarefas) //jogando array atualizada no localStorage
+    
     datas.splice(i, 1)
+    localStorage.arrDate = JSON.stringify(datas)
+    
     concluidas.splice(i, 1) //removendo o estado da tarefa excluida
+    
     exibirTarefas()
     exibirDatas()
+    atualizarContador()
+    tarefaRecente()
+    progresso()
     }
 
 function editarTarefa(i){
@@ -149,17 +187,55 @@ function editarTarefa(i){
         tarefas[i] = tarefaEditada
         exibirTarefas()
         exibirDatas()
+        
+        atualizarContador()
+        tarefaRecente()
+        progresso()
     }
 
 }
 
 function limparTudo (){
-    const mensagemErro2 = document.getElementById("mensagemErro2")
+    
     tarefas.length = 0
+    localStorage.arr = JSON.stringify(tarefas) //jogando array atualizada no localStorage
+    
     datas.length = 0
+    localStorage.arrDate = JSON.stringify(datas)
+    
     concluidas.length = 0
+    
     mensagemErro2.textContent = "Lista limpa com sucesso!"
     mensagemErro2.style.color = "blue"
     exibirTarefas()
     exibirDatas()
+    atualizarContador()
+    tarefaRecente()
+    progresso()
+}
+
+function atualizarContador(){
+    const cont = document.getElementById("cont")
+    cont.textContent = concluidas.filter(Boolean).length + "/" + tarefas.length  //aumenta o contador a cada elemento novo da lista
+}
+
+function tarefaRecente(){
+    const recente = document.getElementById("recente")
+    if (concluidas.filter(Boolean).length == 0){
+        recente.textContent = " - "  
+    }
+    else {
+        recente.textContent = tarefas[tarefas.length - 1] //se as tarefas concluidas forem diferentes de 0, recebe a última da array
+    }
+}
+
+function progresso(){
+    const progresso = document.getElementById("progresso")
+    if (concluidas.filter(Boolean).length == 0){
+        progresso.textContent = "0%"
+    }
+    else {
+        let porcentagem = (concluidas.filter(Boolean).length / tarefas.length) * 100 //tarefas concluidas/todas as tarefas * 100
+        progresso.textContent = porcentagem.toFixed(2) + "%" //número fixo com duas casas decimais
+    }
 }
